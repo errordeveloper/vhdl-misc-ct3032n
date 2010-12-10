7738f9e9b85d57bc8b2587a3026a14d1d6c4bf83
Ilya Dmitrichenko <errordeveloper@gmail.com>
2010-12-10 15:50:29 +0000

    basic PE4 implemented

--- code/comblogic/priority_encoder_4bit.vhd
+++ code/comblogic/priority_encoder_4bit.vhd
@@ -16,14 +16,51 @@ Library IEEE;
 --! PE4 design element.
 Entity PE4 is
   Port (
-        I:	in	std_Logic_Vector ( 3 DOWNTO 0 ); --! PE4 input
-        OUTPUT:	out	std_Logic  --! PE4 output
+        V:	in	std_Logic_Vector ( 3 DOWNTO 0 ); --! Prioritized Inputs
+	A:	out	std_Logic_Vector ( 1 DOWNTO 0 ); --! Address Output
+        X:	out	std_Logic  --! Output
        );
 end entity;
 
 --! @brief Architure definition of the PE4
 --! @details More details about this element.
-architecture logic of PE4 is
+Architecture logic of PE4 is
+
+  signal W: std_Logic_Vector ( 1 DOWNTO 0 );
+  signal Q: std_Logic;
+
 begin
+
+  process ( V )
+  begin
+  
+   case V is
+
+   when "XXX1" =>
+   	Q <= V(0);
+	W <= "00";
+
+   when "XX10" =>
+   	Q <= V(1);
+	W <= "01";
+
+   when "X100" =>
+   	Q <= V(2);
+	W <= "10";
+
+   when "1000" =>
+   	Q <= V(3);
+	W <= "11";
+
+   when others =>
+   	Q <= '0';
+	W <= "XX";
+
+   end case;
+
+  end process;
+
+  A <= W;
+  X <= Q;
 
 end architecture;
